a7d574327df9fa8d0dea23761c8bfb60a779d2a0
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2024-02-06 11:56:23 -0800

    build for custom domain

---
+++ build/precache-manifest.43b26402c43691948a9c4bc7e3a69223.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddab3808ecd951fd9ef6d81d7be9d1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb1b1e2ffe6e26ccf2db",
+    "url": "/static/css/main.c45f443f.chunk.css"
+  },
+  {
+    "revision": "12ccaf1394a3c80d2f99",
+    "url": "/static/js/2.6b0f81ff.chunk.js"
+  },
+  {
+    "revision": "bb1b1e2ffe6e26ccf2db",
+    "url": "/static/js/main.d6f41fc6.chunk.js"
+  },
+  {
+    "revision": "bbd903f12637a98f1d94",
+    "url": "/static/js/runtime~main.2593b0d6.js"
+  }
+]);
